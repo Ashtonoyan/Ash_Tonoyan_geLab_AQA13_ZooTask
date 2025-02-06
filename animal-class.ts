e595127ccb1a8584e53f8207a77438c366ae85d8
Ash <atv007ash@gmail.com>
2025-02-06 21:30:53 +0400

    Initial commit. Created and added a class Animal

---
+++ animal-class.ts
@@ -0,0 +1,9 @@
+export abstract class Animal{
+    constructor(
+        public name: string,
+        public age: number,
+        public species: string,
+        public sound: string
+    ){}
+
+}
